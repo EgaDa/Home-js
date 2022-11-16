e8fc3f4fab8e17f02278c0e490fb91d40eaa0604
EgaDa <egorrimontiv@gmail.com>
2022-11-16 21:18:45 +0500

    leson 1

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var a = +prompt('Сколько вам лет?')
+// var a = +prompt('Сколько вам лет?')
 
-if (a > 0 && a <= 18) {
-    console.log('Вы ещё молоды, вам нужно учиться');
-} else if (a > 18 && a <= 50) {
-    console.log('Вам нужно работать');
-} else if (a > 50 && a <= 59) {
-    console.log('Вам скоро на пенсию');
-} else if (a > 59 && a <= 150) {
-    console.log('Вы пенсионер');
+// if (a > 0 && a <= 18) {
+//     console.log('Вы ещё молоды, вам нужно учиться');
+// } else if (a > 18 && a <= 50) {
+//     console.log('Вам нужно работать');
+// } else if (a > 50 && a <= 59) {
+//     console.log('Вам скоро на пенсию');
+// } else if (a > 59 && a <= 150) {
+//     console.log('Вы пенсионер');
+// } else {
+//     console.log('Что-то пошло не так!');
+// }
+
+
+
+var time = +prompt('Введите любой час суток')
+
+if (time >= 4 && time <= 11) {
+    console.log(time + ' утра');
+} else if (time >= 12 && time <= 16) {
+    console.log(time + ' дня');
+} else if (time >= 17 && time <= 20) {
+    console.log(time + ' вечера');
+} else if (time >= 21 && time <= 3) {
+    console.log(time + ' ночи');
 } else {
     console.log('Что-то пошло не так!');
 }
